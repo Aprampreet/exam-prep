fb302c1df0e7c1a83d660c353b8e470df4597e04
Aprampreet <aprampreetsingh0000@gmail.com>
2026-01-26 23:31:35 +0530

    v1

--- frontend/lib/api.ts
+++ frontend/lib/api.ts
@@ -5,9 +5,7 @@ const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
 export const api = axios.create({
     baseURL: API_BASE_URL,
     withCredentials: true,
-    headers: {
-        "ngrok-skip-browser-warning": "69420", // This bypasses the ngrok interstitial page
-    }
+
 });
 
 
