b514a072885ffe0a25e12b59ee9e1f62363f9409
Aprampreet <aprampreetsingh0000@gmail.com>
2026-02-16 14:27:21 +0530

    main

--- backend/main/main.py
+++ backend/main/main.py
@@ -3,7 +3,7 @@ import uvicorn
 if __name__ == "__main__":
     uvicorn.run(
         "app.app:app",
-        host="[IP_ADDRESS]",
+        host="0.0.0.0",
         port=8080,
         reload=True,
     )
