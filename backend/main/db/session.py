d2c522ab69b3ff01903db80c4c498ac39ba6f4d1
Aprampreet <aprampreetsingh0000@gmail.com>
2026-02-21 14:58:45 +0530

    add

--- backend/main/db/session.py
+++ backend/main/db/session.py
@@ -3,7 +3,7 @@ from app.core.config import settings
 
 engine = create_async_engine(
     settings.DATABASE_URL,
-    echo=False,
+    echo=True,
     pool_pre_ping=True,
     connect_args={
         "statement_cache_size": 0
--- backend/main/main.py
+++ backend/main/main.py
@@ -5,5 +5,6 @@ if __name__ == "__main__":
         "app.app:app",
         host="localhost",
         port=8080,
+        
         reload=True,
     )
